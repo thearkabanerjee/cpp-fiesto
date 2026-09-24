61e5a56009e15e4a134e7f9e3335a2d4d8535bdf
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-24 10:09:01 +0530

    very basic

---
+++ verybasic/helloworld.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!"<< endl;
+
+    // this is a comment as usual
+    /*
+        another one
+    */
+    return 0;
+}
