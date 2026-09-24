5ebcbe4db69530f18e727b12d6fb38d749fff71a
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-24 10:38:16 +0530

    very basic

---
+++ verybasic/variable.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 6;
+    int sum = a+b;
+    cout << a << endl;
+    cout << b << endl;
+
+    cout << sum << endl;
+
+    return 0;
+}
