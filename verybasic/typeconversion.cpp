153db1d46eac8a43c7c7188633a5b2b24bace3fb
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-25 10:51:45 +0530

    type conversion

---
+++ verybasic/typeconversion.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+  int a = 7;
+  int b = 2;
+
+  
+  cout << a/ b << endl; // this is the normal integer division
+  cout <<(float) a / b << endl; // this is the type changed version
+  
+
+  return 0;
+}
