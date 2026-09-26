6a57b8581207e4af1b6f9bfffc4c9e6cabe08e84
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-26 09:21:12 +0530

    temperature-conversion.cpp

---
+++ questions/temperture-conversion.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+  int temp;
+  cout << "enter your temp in Celsius: ";
+  cin >> temp;
+
+  int ferhen= ((9* temp)/ 5) + 32;
+
+
+  cout << ferhen << endl;
+  return 0;
+}
